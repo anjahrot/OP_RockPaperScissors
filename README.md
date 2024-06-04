a4a590e87069953b13efecf68d82b822e133b135
anjahrot <157907978+anjahrot@users.noreply.github.com>
2024-06-04 18:37:13 +0200

    Added readme.md, index.js and game.js to start the project. Started getComputerChoice function using math.random to give values 0,1 or 2

---
+++ README.md
@@ -0,0 +1,2 @@
+This is one of my projects for the Odin Project. To make a game of 
+Rock, paper, scissors to be played in the console.
---
+++ game.js
@@ -0,0 +1,24 @@
+console.log("Hello World!")
+
+//computerChoice - get with Math.random()
+function getComputerChoice() {
+    let number = Math.floor(Math.random()*3);
+    console.log(number);
+}
+
+getComputerChoice();
+
+//humanChoice - prompt input from user - function getHumanChoice
+
+//Declare players score variables humanScore and computerScore (initVal = 0)
+
+//Function playRound that takes humanChoice (parameter case insensitive) and computerChoice as arguments
+//console.log the winner and result
+//increment Score values
+
+//const humanSelection = getHumanChoice();
+//const computerSelection = getComputerChoice();
+
+//playRound(humanSelection, computerSelection);
+
+//function playGame - five rounds, 
