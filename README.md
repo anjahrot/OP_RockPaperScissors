c0318cb9e24d692c9fb1960e3065b5134830dee7
anjahrot <157907978+anjahrot@users.noreply.github.com>
2024-06-06 15:15:02 +0200

    "Edit game.js further to add content to DOM, displaying results in the browserwindow as the game proceeds"

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 This is one of my projects for the Odin Project. To make a game of 
-Rock, paper, scissors to be played in the console.
+Rock, paper, scissors first to be played in the console. In this branch I 
+will rewrite the code to add an UI for a player to play the game in the browser.
--- game.js
+++ game.js
@@ -14,14 +14,6 @@ function getComputerChoice() {
     return compChoice;
 }
 
-//humanChoice - prompt input from user - function getHumanChoice
-function getHumanChoice() {
-    let humanChoice = prompt('Rock, paper or scissors? ');
-    humanChoice = humanChoice.toLowerCase();
-    return humanChoice;
-}
-
-
 //Declare players score variables humanScore and computerScore (initVal = 0)
 let humanScore = 0;
 let compScore = 0;
@@ -29,23 +21,27 @@ let compScore = 0;
 //Function playRound that takes humanChoice (parameter case insensitive) and computerChoice as arguments
 //console.log the winner and result
 function playRound(humanChoice, compChoice){
-      if (humanChoice === compChoice){
-       result.textContent = `It is a tie! Both players chose ${humanChoice}. `
+      if (humanScore === 5) gameResult.textContent = 'You win!'
+      else if (compScore === 5) gameResult.textContent = 'Computer wins!'
+      else if (humanChoice === compChoice){
+       roundResult.textContent = `It is a tie! Both players chose ${humanChoice}. `
       }
       else if (humanChoice === "rock" && compChoice === "scissors" || humanChoice === "scissors" 
         && compChoice === "paper" || humanChoice === "paper" && compChoice === "rock" ){
-        result.textContent = `You win! ${humanChoice} beats ${compChoice}.`;
+        roundResult.textContent = `You win! ${humanChoice} beats ${compChoice}.`;
         //increment humanScore value if player wins
         humanScore++;
+        playerScore.textContent = `Points: ${humanScore} `;
       }
       else {
-        result.textContent = `You loose! ${compChoice} beats ${humanChoice}`;
+        roundResult.textContent = `You loose! ${compChoice} beats ${humanChoice}`;
         //increment compScore value if comp wins
         compScore++;
+        computerScore.textContent = `Points: ${compScore} `;
       }
    }
 
-//Adding buttons for player to select choice
+//DOM - Adding buttons for player to select choice
 const btnRock = document.createElement("button");
 btnRock.textContent = 'Rock';
 const btnPaper = document.createElement("button");
@@ -58,13 +54,23 @@ buttons.appendChild(btnRock);
 buttons.appendChild(btnPaper);
 buttons.appendChild(btnScissors);
 
-//Addding heading
+//Add heading at the top of the page
 const heading = document.createElement("h1");
 heading.textContent = "Hello! Ready to play a game? ";
 document.body.insertBefore(heading, buttons);
 
 //Adding div to display results
 const result = document.createElement("div");
+const roundResult = document.createElement("h3");
+const scores = document.createElement("ul");
+const playerScore = document.createElement("li");
+const computerScore = document.createElement("li");
+const gameResult = document.createElement("h2");
+result.appendChild(roundResult);
+scores.appendChild(playerScore);
+scores.appendChild(computerScore);
+result.appendChild(scores);
+result.appendChild(gameResult);
 document.body.appendChild(result);
 
 //Single-eventhandler to handle all click-events
